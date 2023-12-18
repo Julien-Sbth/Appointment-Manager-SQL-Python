341f0da814fad2071034dc9cb3fe738283755a85
mamadou <amadou.mbodj@ynov.com>
2023-12-18 16:50:17 +0100

    Changes by Julien

--- RendezVous/Patients.py
+++ RendezVous/Patients.py
@@ -36,3 +36,61 @@ class Patient:
             print("Le patient a été ajouté à la base de données avec succès.")
         except sqlite3.Error as e:
             print(f"Erreur lors de l'ajout du patient : {e}")
+
+    def remove_from_database(self):
+            try:
+                connection = sqlite3.connect('database.sqlite')
+                cursor = connection.cursor()
+
+                cursor.execute('''
+                    DELETE FROM patient
+                    WHERE prenom = ? AND nom = ? AND age = ? AND sexe = ?
+                ''', (self.prenom, self.nom, self.age, self.sexe))
+
+                connection.commit()
+                connection.close()
+                print("Le patient a été supprimé de la base de données avec succès.")
+            except sqlite3.Error as e:
+                print(f"Erreur lors de la suppression du patient : {e}")
+
+    @staticmethod
+    def Modif_Patient(prenom, nom, age, sex):
+        try:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                                    UPDATE patient
+                                    SET nom = ?, age = ?, sex = ?
+                                    WHERE prenom = ?
+                                ''', (nom, age, sex, prenom))
+
+            connection.commit()
+            connection.close()
+            print("Les informations du patient ont été mises à jour avec succès.")
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la mise à jour des informations du patient : {e}")
+
+    @staticmethod
+    def AllDisplayDataFromPatient():
+        try:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                SELECT SecretaireID,Nom,Prenom FROM patient
+            ''')
+
+            rows = cursor.fetchall()
+            connection.close()
+
+            if rows:
+                print("Liste des secretaires :")
+                for row in rows:
+                    print(list(row))
+                return rows
+            else:
+                print("La table 'secretaire' est vide.")
+                return []
+        except sqlite3.Error as e:
+            print(f"Erreur lors de l'affichage des secretaires : {e}")
--- main.py
+++ main.py
@@ -5,34 +5,5 @@ import interface
 def main():
     interface.interfaceSecretaire()
 
-while True:
-    print("1 Pour ajouter un patient")
-    print("2 Pour prendre un rendez-vous")
-    print("3 Pour modifier un rendez-vous")
-    print("4 Pour supprimer un rendez-vous")
-    choix = input("Entrez votre choix (ou 'q' pour quitter) : ")
 
-    if choix == "1":
-        def ajouter_patient():
-            print("Entrez les détails du nouveau patient :")
-            Prenom = input("Prenom du patient : ")
-            nom = input("Nom du patient : ")
-            age = int(input("age du patient : "))
-            condition = input("sexe du patient : ")
-
-            nouveau_patient = Patient(Prenom, nom, age, condition)
-            nouveau_patient.add_to_database()
-
-        ajouter_patient()
-
-    elif choix == "2":
-        pass
-    elif choix == "3":
-        pass
-    elif choix == "4":
-        pass
-
-    elif choix.lower() == "q":
-        break
-    else:
-        print("Choix invalide. Veuillez entrer un choix valide.")
+main()
