2d013a552fb3a8cbe573e7b8b964064f508ee6c6
JS <julien.sabathe@ynov.com>
2023-12-18 13:28:23 +0100

    [ADD Patient To Database]

--- RendezVous/Patients.py
+++ RendezVous/Patients.py
@@ -1,4 +1,38 @@
+# patient.py
 
-class Patients:
-    def __init__(self,):
+import sqlite3
 
+class Patient:
+    def __init__(self, prenom, nom, age, sex):
+        self.prenom = prenom
+        self.nom = nom
+        self.age = age
+        self.sex = sex
+
+    def add_to_database(self):
+        try:
+            connection = sqlite3.connect('database.sqlite')  # Chemin relatif pour la base de données
+            cursor = connection.cursor()
+
+            # Création de la table si elle n'existe pas déjà
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS patient (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    prenom TEXT NOT NULL,
+                    nom TEXT NOT NULL,
+                    age INTEGER NOT NULL,
+                    sex TEXT NOT NULL
+                )
+            ''')
+
+            # Insertion du patient dans la base de données
+            cursor.execute('''
+                INSERT INTO patient (prenom, nom, age, sex)
+                VALUES (?, ?, ?, ?)
+            ''', (self.prenom, self.nom, self.age, self.sex))
+
+            connection.commit()
+            connection.close()
+            print("Le patient a été ajouté à la base de données avec succès.")
+        except sqlite3.Error as e:
+            print(f"Erreur lors de l'ajout du patient : {e}")
--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-import sqlite3
+# main.py
 
-from RendezVous.PrendreRendezVous import RendezVous
-
-try:
-    conn = sqlite3.connect('database.sqlite')
-    print("Connexion réussie!")
-
-    cursor = conn.cursor()
-
-
-    Patient = ('Alice', 30, 'test')
-
-    cursor.execute("INSERT INTO Patient (Prenom, Nom, Age) VALUES (?, ?, ?)", Patient)
-
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-except sqlite3.Error as e:
-    print("Erreur lors de la connexion à la base de données:", e)
+from RendezVous.Patients import Patient
 
 while True:
-    while True:
-        print("1 Pour prendre un rendez-vous")
-        print("2 Pour modifier un rendez vous")
-        print("3 Pour supprimer un rendez-vous")
-        choix = input("Entrez votre choix (ou 'q' pour quitter) : ")
+    print("1 Pour ajouter un patient")
+    print("2 Pour prendre un rendez-vous")
+    print("3 Pour modifier un rendez-vous")
+    print("4 Pour supprimer un rendez-vous")
+    choix = input("Entrez votre choix (ou 'q' pour quitter) : ")
 
-        if choix == "1":
-            RendezVous.prendre_rendezvous()  # Définit par sa date, son heure, le patient concerner  le nom du
-            # médecin, la secrétaire qui enregistre le rendez-vous
-        elif choix == "2":
-            RendezVous.Modification()
-        elif choix == "3":
-            RendezVous.Supression()
-        elif choix == "q":
-            print("au revoir")
+    if choix == "1":
+        def ajouter_patient():
+            print("Entrez les détails du nouveau patient :")
+            Prenom = input("Prenom du patient : ")
+            nom = input("Nom du patient : ")
+            age = int(input("age du patient : "))
+            condition = input("sexe du patient : ")
+
+            nouveau_patient = Patient(Prenom, nom, age, condition)
+            nouveau_patient.add_to_database()
+
+        ajouter_patient()
+
+    elif choix == "2":
+        pass
+    elif choix == "3":
+        pass
+    elif choix == "4":
+        pass
+
+    elif choix.lower() == "q":
         break
-
+    else:
+        print("Choix invalide. Veuillez entrer un choix valide.")
