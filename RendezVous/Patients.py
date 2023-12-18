3a6197be208fd252f41c41b85acaeb9799d3ad11
JS <julien.sabathe@ynov.com>
2023-12-18 21:48:06 +0100

    [Update Files]

--- RendezVous/Patients.py
+++ RendezVous/Patients.py
@@ -36,3 +36,64 @@ class Patient:
             print("Le patient a été ajouté à la base de données avec succès.")
         except sqlite3.Error as e:
             print(f"Erreur lors de l'ajout du patient : {e}")
+
+    def remove_from_database(self):
+        try:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                DELETE FROM patient
+                WHERE prenom = ?
+            ''', (self.prenom,))
+
+            connection.commit()
+            connection.close()
+            print("Le patient a été supprimé de la base de données avec succès.")
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la suppression du patient : {e}")
+
+    @staticmethod
+    def Modif_Patient(prenom, nom, age, sex):
+        try:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                                UPDATE patient
+                                SET nom = ?, age = ?, sex = ?
+                                WHERE prenom = ?
+                            ''', (nom, age, sex, prenom))
+
+            connection.commit()
+            connection.close()
+            print("Les informations du patient ont été mises à jour avec succès.")
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la mise à jour des informations du patient : {e}")
+    @staticmethod
+    def AllDisplayDataFromPatient():
+        try:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                    SELECT PatientID, prenom, nom FROM patient
+                ''')
+
+            rows = cursor.fetchall()
+            connection.close()
+
+            if rows:
+                formatted_data = [list(row) for row in rows]  # Convertit chaque tuple en liste
+                for data in formatted_data:
+                    print(data)  # Affiche chaque liste de données
+                return formatted_data  # Retourne les données formatées
+            else:
+                print("La table 'patient' est vide.")
+                return []  # Retourne une liste vide si aucune donnée trouvée
+        except sqlite3.Error as e:
+            print(f"Erreur lors de l'affichage des patients : {e}")
+
+
+
+
--- main.py
+++ main.py
@@ -23,9 +23,20 @@ while True:
         ajouter_patient()
 
     elif choix == "2":
-        pass
+        Patient.AllDisplayDataFromPatient()
+
+        prenom_patient = input("Entrez le prénom du patient à supprimer : ")
+        patient_a_supprimer = Patient(prenom_patient, "", 0, "")
+        patient_a_supprimer.remove_from_database()
     elif choix == "3":
-        pass
+        Patient.AllDisplayDataFromPatient()
+
+        prenom_a_modifier = input("Entrez l'id du patient à modifier : ")
+        nouveau_nom = input("Entrez le nouveau nom : ")
+        nouvel_age = input("Entrez le nouvel âge : ")
+        nouveau_sexe = input("Entrez le nouveau sexe : ")
+
+        Patient.Modif_Patient(prenom_a_modifier, nouveau_nom, nouvel_age, nouveau_sexe)
     elif choix == "4":
         pass
 
